a6f6fa568eb902929df3e0208b547908ce56ec26
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-12 21:19:36 +0530

    Add passkey modal for admin verification

---
+++ app/admin/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Admin = () => {
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={162}
+            height={32}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome Back, Admin</h1>
+          <p className="text-dark-700">
+            Manage New and Existing Appointments Here.
+          </p>
+        </section>
+        <section className="admin-stat"></section>
+      </main>
+    </div>
+  );
+};
+
+export default Admin;
